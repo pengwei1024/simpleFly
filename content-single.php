71e1979948b6483fca12ad93d15e701ba44f6178
pengwei1024 <pengwei1024@gmail.com>
2015-09-18 10:09:08 +0800

    修复上一篇下一篇样式

--- content-single.php
+++ content-single.php
@@ -52,10 +52,7 @@
     ?>
     <div id="comment-area">
         <div class="pn_post_link">
-            <?php
-            previous_post_link('%link', '<b>上一篇:</b><span>%title</span><br/>');
-            next_post_link('%link', '<b>下一篇:</b><span>%title</span>');
-            ?>
+            <?php previous_post_link('%link', '<b>上一篇:</b><span>%title</span>');next_post_link('%link', '<b>下一篇:</b><span>%title</span>'); ?>
         </div>
         <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a><a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a><a href="#" class="bds_twi" data-cmd="twi" title="分享到Twitter"></a></div>
         <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
